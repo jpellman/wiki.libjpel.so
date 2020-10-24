ec093ef7f5ed582f695dfea02909c0481c24b323
John Pellman <pellman.john@gmail.com>
2020-10-24 19:47:05 -0400

    argparsify

--- scripts/README.md
+++ scripts/README.md
@@ -1,3 +1,14 @@
  * Gollum/Grit/Rugged uses its own implementation of Git, which only respects Git hooks that are coded into a Ruby config file in some way (see [here](https://github.com/gollum/gollum/issues/69#issuecomment-281501178)).  This necessitates running `gollum --config ./scripts/config.rb`.
  * `build_subpages.py` needs to be run once every time the repo is cloned to install itself as a post-commit hook (or it needs to be added as a post-commit hook manually).
  * `build_subpages.py` requires a Python 3 environment with GitPython installed.
+
+pandoc-bootstrap-template
+=========================
+
+Bootstrap template for Pandoc - Converts markdown files into Twitter Bootstrap styled HTML
+
+This version has been modified so that the file fed into `--include-before-body` is placed in a sidebar on the righthand side, and the TOC is placed before the body of the main text.
+
+### Usage example
+
+    pandoc -f markdown -t html -o Charities.html --template=template.html --include-after-body=_Footer.html --include-before-body=_Sidebar.html --toc --toc-depth=2 --metadata title=Charities --css template.css --self-contained Charities.md
--- scripts/build.py
+++ scripts/build.py
@@ -5,27 +5,23 @@ from git import Repo
 from glob import glob
 import sys
 
-def getChangedCategories(gitRepo):
+def getCategoryDict(gitRepoRoot, fileList):
     '''
-    A function that finds only recently changed markdown pages and then returns
+    A function that iterates throught a list of files and then returns
     a dictionary with the directories for changed categories as keys, and a list 
     containing the pages belonging to those categories as values.
     '''
-    # Only look at recently modified pages.
-    lastModifiedFiles = list(gitRepo.head.commit.stats.files.keys())
-    gitRepoRoot = gitRepo.working_tree_dir
-
     categoryDict = {}
     for fi in lastModifiedFiles:
         # Ignore scripts directory.
         if os.path.dirname(fi) == "scripts":
             continue
         if "=>" in fi:
-            fi = fi.split(">")[1].strip("})
+            fi = fi.split(">")[1].strip("}")
         if not os.path.isdir(os.path.join(gitRepoRoot, os.path.dirname(fi))):
             continue
         # Remove hidden files, non-markdown files, and Gollum subpages from consideration.
-        if (fi.startswith('.') or fi.startswith('_')  and not fi.endswith('.md')):
+        if (fi.startswith('.') or fi.startswith('_') or fi == "Home.md" and not fi.endswith('.md')):
             continue
         categoryPages = set(glob(os.path.join(gitRepoRoot, os.path.dirname(fi), '*.md')))
         # Needed in case the category is brand new.
@@ -66,13 +62,43 @@ def makeFooter(categoryDir, gitRepoRoot):
     return ' **>** '.join(footerList)
 
 if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Generate HTML with footers/headers from multiple markdown files using pandoc")
+    parser.add_argument("-i", "--initialize", action="store_true", help="Regenerate all header and footer files.")
+    parser.add_argument("-u", "--update", action="store_true", help="Regenerate only header and footer files for categories that were updated in the last commit.")
+    args = parser.parse_args()
+
+    # Sanity checks
+    if args.initialize and args.update:
+        print("You cannot both initialize and update the headers/footers.")
+        print("Exiting now.")
+        sys.exit(1)
+    if not args.initialize and not args.update:
+        print("You must specify an initialization or update operation.")
+        print("Exiting now.")
+        sys.exit(1)
+   
     # Create the git repo object.  Find the root of the git repository and make an index object.
     gitRepo = Repo('.', search_parent_directories=True)
-    gitIndex = gitRepo.index
     gitRepoRoot = gitRepo.working_tree_dir
 
-    # Get all changed categories and the pages that belong to them.
-    categories = getChangedCategories(gitRepo)
+    if args.initialize:
+        fileList = []
+        for root, dirname, filenames in os.walk(gitRepoRoot):
+            if os.path.join(gitRepoRoot,'.git') in root or os.path.join(gitRepoRoot,'scripts') in root:
+                continue
+            else:
+                for filename in filenames:
+                    fileList.append(os.path.join(root,filename))
+    
+    # Only look at recently modified pages.
+    if args.update:
+        fileList= list(gitRepo.head.commit.stats.files.keys())
+    print(fileList)
+    sys.exit(0)
+
+    categories = getCategoryDict(gitRepoRoot, fileList)
 
     for categoryDir,categoryPages in categories.items():
         # Make a footer for the changed category.
